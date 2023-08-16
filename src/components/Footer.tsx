4e0354657d4da49876a89dd1029f41b4bd1876a9
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-08-16 14:41:14 +0100

    setup strcuture of home

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+        <h2>✈️ Your Next Adventure Awaits!</h2>
+        <p>Dive deep into our travel guides, tips, and tales. Whether you're looking for the next best place to dine in Paris, or trekking routes in the Andes, we've got you covered. So grab your map, pack your bags, and let's embark on a journey together!</p>
+        <p><em>Discover the World, One Adventure at a Time.</em></p>
+    </footer>
+  )
+}
