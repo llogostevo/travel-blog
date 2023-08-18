09a70b552a6c2734dcb13635c73a488155a83001
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-08-18 11:30:26 +0100

    added post arrays

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -48,7 +48,7 @@ export default function BlogPostPage({ params }: BlogPostParams) {
         <main>
             <div className='px-20 border bg-[#fafef] shadow rounded-lg mb-10'>
                 <section className='mb-20'>
-                
+
                     <h1 className={`${dancing_script.className} text-4xl md:text-5xl font-bold -skew-y-3`} >{post?.title}</h1>
                     <h2 className={`${dancing_script.className} text-2xl mt-2 font-bold -skew-y-3`} >{post?.month} {post?.year}</h2>
                 </section>
@@ -67,32 +67,17 @@ export default function BlogPostPage({ params }: BlogPostParams) {
                         />
                     </div>
                     <div>
-                        <p>{post?.content}</p>
-                        {/* THIS IS STATIC CONTENT AT THE MOMENT put this into an object */}
-                        <br />
-                        <p className="italic underline">this is static below</p>
-                        <h2 className="font-bold text-lg mb-4">✈️ A Whirlwind Journey Through Berlin! ✈️</h2>
-
-                        <p className="mb-2 ml-10">Berlin, with its rich history and vibrant cultural scene, had always been on my bucket list. Last month, I finally took the plunge and embarked on a week-long adventure to this German metropolis.</p>
-
-                        <h3 className="font-medium text-md mt-4 mb-2 ml-10">Day 1: Touchdown in Berlin</h3>
-                        <p className="ml-10 mb-4">As the plane descended into Tegel Airport, the iconic Fernsehturm (TV Tower) pierced the Berlin skyline, beckoning me to explore the city. After settling into my cozy Airbnb in the Kreuzberg district, I decided to stroll around and get a feel for the neighborhood. Streets filled with quirky art galleries, eclectic bars, and tantalizing food stalls welcomed me. Dinner was at a local Döner kebab shop – an absolute must-try when in Berlin.</p>
-
-                        <h3 className="font-medium text-md mt-4 mb-2 ml-10">Day 2: History Comes Alive</h3>
-                        <p className="ml-10 mb-4">The day was dedicated to the city&apos;s turbulent history. I started at the Brandenburg Gate, a symbol of unity and peace, before heading to the Berlin Wall Memorial. Walking alongside remnants of the Wall, I felt the weight of Berlin&apos;s divided past. The Checkpoint Charlie Museum provided further insights into the Cold War era.</p>
-
-                        <h3 className="font-medium text-md mt-4 mb-2 ml-10">Day 3: Museum Island & Modern Art</h3>
-                        <p className="ml-10 mb-4">Berlin boasts an impressive collection of museums. I began at Museum Island, losing myself in the ancient treasures of the Pergamon Museum. Later, the contemporary art at the Hamburger Bahnhof museum left me both intrigued and inspired.</p>
-
-                        <h3 className="font-medium text-md mt-4 mb-2 ml-10">Day 4: Venturing Beyond the City</h3>
-                        <p className="ml-10 mb-4">A short train ride took me to Potsdam, home to the exquisite Sanssouci Palace. The palace gardens in spring bloom were a sight to behold. Returning to Berlin, I dined at a riverside restaurant, watching the sun set over the Spree.</p>
-
-                        <h3 className="font-medium text-md mt-4 mb-2 ml-10">Day 5: The Vibrant Berlin Nightlife</h3>
-                        <p className="ml-10 mb-4">The day was reserved for relaxation at Tiergarten, Berlin&apos;s expansive central park. But as night fell, Berlin&apos;s famed nightlife beckoned. I danced the night away at a techno club in Friedrichshain, truly immersing myself in Berlin&apos;s modern beats.</p>
-
-                        <h3 className="font-medium text-md mt-4 mb-2 ml-10">Final Thoughts:</h3>
-                        <p className="ml-10">Berlin proved to be a delightful blend of old and new. Its resilience and transformation are evident in every corner. From its haunting history to its avant-garde art scene, Berlin promises an enriching experience for every traveler.</p>
-                    </div>
+                        
+                        <div key={post.slug}>
+                            {/* <h2 className="font-bold text-lg mb-4">{post.title}</h2> */}
+                            {post.blogTitle.map((title, index) => (
+                                <div key={index} className="my-4"> {/* "my-4" is for margin on the y-axis (top & bottom) */}
+                                    <h3 className={`${dancing_script.className} font-bold  text-2xl mt-4 mb-2 ml-3`}>{title}</h3>
+                                    <p className="ml-10 mb-4">{post.blogDescription[index]}</p>
+                                </div>
+                            ))}
+                        </div>
+                       </div>
 
                 </section>
             </div>
--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -6,6 +6,17 @@ const POSTS = [
         month: 'August',
         year: '2023',
         category: "interailing",
+        blogTitle:['✈️ A Whirlwind Journey Through Berlin! ✈️',
+    'Day 1: Touchdown in Berlin','Day 2: History Comes Alive','Day 3: Museum Island & Modern Art','Day 4: Venturing Beyond the City','Day 5: The Vibrant Berlin Nightlife', 'Final Thoughts:'],
+        blogDescription:['Berlin, with its rich history and vibrant cultural scene, had always been on my bucket list. Last month, I finally took the plunge and embarked on a week-long adventure to this German metropolis.',
+        'As the plane descended into Tegel Airport, the iconic Fernsehturm (TV Tower) pierced the Berlin skyline, beckoning me to explore the city. After settling into my cozy Airbnb in the Kreuzberg district, I decided to stroll around and get a feel for the neighborhood. Streets filled with quirky art galleries, eclectic bars, and tantalizing food stalls welcomed me. Dinner was at a local Döner kebab shop – an absolute must-try when in Berlin.',
+        'The day was dedicated to the city&apos;s turbulent history. I started at the Brandenburg Gate, a symbol of unity and peace, before heading to the Berlin Wall Memorial. Walking alongside remnants of the Wall, I felt the weight of Berlin&apos;s divided past. The Checkpoint Charlie Museum provided further insights into the Cold War era.',
+        'Berlin boasts an impressive collection of museums. I began at Museum Island, losing myself in the ancient treasures of the Pergamon Museum. Later, the contemporary art at the Hamburger Bahnhof museum left me both intrigued and inspired.',
+        'A short train ride took me to Potsdam, home to the exquisite Sanssouci Palace. The palace gardens in spring bloom were a sight to behold. Returning to Berlin, I dined at a riverside restaurant, watching the sun set over the Spree.',
+        'The day was reserved for relaxation at Tiergarten, Berlin&apos;s expansive central park. But as night fell, Berlin&apos;s famed nightlife beckoned. I danced the night away at a techno club in Friedrichshain, truly immersing myself in Berlin&apos;s modern beats.',
+        'Berlin proved to be a delightful blend of old and new. Its resilience and transformation are evident in every corner. From its haunting history to its avant-garde art scene, Berlin promises an enriching experience for every traveler.'
+    ],
+
     },
     {
         title: "Brussels", 
@@ -14,6 +25,8 @@ const POSTS = [
         month: 'August',
         year: '2023',
         category: "interailing", 
+        blogTitle:[],
+        blogDescription:[]
     },
     {
         title: "Chicago", 
@@ -21,7 +34,9 @@ const POSTS = [
         content: "this is my blog",
         month: 'October',
         year: '2022',
-        category: "sport", 
+        category: "sport",
+        blogTitle:[],
+        blogDescription:[] 
     },
     {
         title: "Cologne", 
@@ -29,7 +44,9 @@ const POSTS = [
         content: "this is my blog",
         month: 'August',
         year: '2023',
-        category: "interailing", 
+        category: "interailing",
+        blogTitle:[],
+        blogDescription:[] 
     },
     {
         title: "Lausanne", 
@@ -37,7 +54,9 @@ const POSTS = [
         content: "this is my blog",
         month: 'August',
         year: '2023',
-        category: "interailing", 
+        category: "interailing",
+        blogTitle:[],
+        blogDescription:[] 
     },
     {
         title: "Milan", 
@@ -45,7 +64,9 @@ const POSTS = [
         content: "this is my blog",
         month: 'August',
         year: '2023',
-        category: "interailing", 
+        category: "interailing",
+        blogTitle:[],
+        blogDescription:[] 
     },
     {
         title: "Paris", 
@@ -53,7 +74,9 @@ const POSTS = [
         content: "this is my blog",
         month: 'August',
         year: '2023',
-        category: "interailing", 
+        category: "interailing",
+        blogTitle:[],
+        blogDescription:[] 
     },
     {
         title: "Venice", 
@@ -61,7 +84,9 @@ const POSTS = [
         content: "this is my blog",
         month: 'August',
         year: '2023',
-        category: "interailing", 
+        category: "interailing",
+        blogTitle:[],
+        blogDescription:[] 
     },
     {
         title: "Vienna", 
@@ -70,6 +95,8 @@ const POSTS = [
         month: 'August',
         year: '2023',
         category: "interailing",
+        blogTitle:[],
+        blogDescription:[]
     },
 ]
 
