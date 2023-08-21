879a7275e6b220c3214307992578129143199e3f
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-08-21 16:02:53 +0100

    fixing env

--- config.ts
+++ config.ts
@@ -1,5 +1,2 @@
 export const WEBSITE_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"
 
-// issues with this
-
-// need this for travel-blog-blush.vercel.app
