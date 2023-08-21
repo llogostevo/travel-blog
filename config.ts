91249adc705e32ea49d5cc203074a6206afee3ac
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-08-21 15:43:48 +0100

    fix for try catch url

--- config.ts
+++ config.ts
@@ -1,3 +1,5 @@
 export const WEBSITE_URL = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"
 
 // issues with this
+
+// need this for travel-blog-blush.vercel.app
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,38 +1,45 @@
-import { WEBSITE_URL} from "config"
+import { WEBSITE_URL } from "config"
 
 import { dancing_script } from "@/app/layout";
 
 export default async function Comments({ slug }: { slug: string }) {
-    const commentsRes = await fetch(`${WEBSITE_URL}/api/comments/${slug}`, { next: { revalidate: 5 } });
-    const comments = await commentsRes.json();
+    let comments = [];
+
+    try {
+        const commentsRes = await fetch(`${WEBSITE_URL}/api/comments/${slug}`, { next: { revalidate: 5 } });
+        comments = await commentsRes.json();
+    } catch (err) {
+        console.log(err)
+    }
+
 
     return (
         <div className="mx-auto px-4 sm:px-6 md:w-3/4  lg:w-2/3 xl:w-1/2">
-    <h3 className={`${dancing_script.className} text-6xl font-bold text-teal -skew-y-3 tracking-tighter m-4`}>Comments</h3>
-    <form className="w-full" method="POST" action={`/api/comments/${slug}`}>
-        <div className="flex flex-col gap-4 items-center border-b border-teal py-2">
-            <input className="appearance-none bg-transparent border-b border-teal py-2 w-full text-gray-700 mr-3 px-2 leading-tight focus:outline-none" type="text" placeholder="Name" name="username" aria-label="Full name" />
-            <textarea className="appearance-none bg-transparent border-none sm:30 md:h-60 lg:h-80 w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="Your comment..." name="comment" aria-label="Comment"></textarea>
-            <button className="flex-shrink-0 bg-teal hover:bg-nonphotblue hover:bg-photo text-sm text-white py-1 px-2 rounded" type="submit">
-                Add Comment
-            </button>
+            <h3 className={`${dancing_script.className} text-6xl font-bold text-teal -skew-y-3 tracking-tighter m-4`}>Comments</h3>
+            <form className="w-full mt-10" method="POST" action={`/api/comments/${slug}`}>
+                <div className="flex flex-col gap-4 items-center border-b border-teal py-2">
+                    <input className="appearance-none bg-transparent border-b border-teal py-2 w-full text-gray-700 mr-3 px-2 leading-tight focus:outline-none" type="text" placeholder="Name" name="username" aria-label="Full name" />
+                    <textarea className="appearance-none bg-transparent border-none sm:30 md:h-60 lg:h-80 w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="Your comment..." name="comment" aria-label="Comment"></textarea>
+                    <button className="flex-shrink-0 bg-teal hover:bg-nonphotblue hover:bg-photo text-sm text-white py-1 px-2 rounded" type="submit">
+                        Add Comment
+                    </button>
+                </div>
+            </form>
+
+            <ul className="w-full">
+                {/* @ts-ignore */}
+                {comments.map((comment) => {
+                    return (
+                        <li key={comment.uuid} className="flex flex-col gap-4 items-start border-b border-teal py-2">
+                            <span className="font-semibold text-gray-700">{comment.username} says:</span>
+                            <p className="text-gray-700 mr-3 px-2">{comment.comment}</p>
+                        </li>
+                    );
+                })}
+            </ul>
+
         </div>
-    </form>
-    
-    <ul className="w-full">
-        {/* @ts-ignore */}
-        {comments.map((comment) => {
-            return (
-                <li key={comment.uuid} className="flex flex-col gap-4 items-start border-b border-teal py-2">
-                    <span className="font-semibold text-gray-700">{comment.username} says:</span>
-                    <p className="text-gray-700 mr-3 px-2">{comment.comment}</p>
-                </li>
-            );
-        })}
-    </ul>
 
-</div>
 
-    
     )
 }
