e69f268215e83f2190da500c4f5b5b578dec71a5
Lloyd Stevens <stevens.lloyd@gmail.com>
2023-08-22 14:25:39 +0100

    setup client side components

---
+++ src/components/CommentForm.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+// import the set website url so that it can be used in vercel
+import { WEBSITE_URL } from "@/../config";
+
+// import use router, make sure it is from next/navigation
+import { useRouter } from 'next/navigation'
+
+import { useTransition } from "react";
+
+export default function CommentForm({ slug }: { slug: string }) {
+    // `/api/comments/${slug}`
+
+    // create the router hook to trigger a page refresh
+    const router = useRouter()
+
+    // create the transition hook to manage the transition of the page refresh
+    const [isPending, startTransition] = useTransition()
+
+    // async function to handle the form submission
+    async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+        // prevent refresh of the page on submit
+        event.preventDefault();
+        console.log(event)
+
+        // get the values from the form
+        //  @ts-ignore
+        const username = event.target.username.value;
+        //  @ts-ignore
+        const comment = event.target.comment.value;
+
+        // create form data object to handle the form data
+        const formData = new FormData();
+        console.log(formData)
+
+        // append the data in the form to the form data object
+        formData.append("username", username)
+        formData.append("comment", comment)
+
+        //make the api call
+            // set the options that will be used in the fetch
+            const options = {body: formData, method: "POST"}
+            
+            // use the fetch API to action the post method containing the form data
+            // the formData object handles the headers for the fetch api
+            const res = await fetch(`${WEBSITE_URL}/api/comments/${slug}`, options)
+
+    
+            // refresh the current route and fetch new data from the server without losing the client side browser or react state
+            // takes a callback as a parameter
+            startTransition(() => {
+                router.refresh();
+
+            })
+
+    }
+
+    return (
+        <form className="w-full mt-10" onSubmit={handleFormSubmit} >
+            <div className="flex flex-col gap-4 items-center border-b border-teal py-2">
+                <input className="appearance-none bg-transparent border-b border-teal py-2 w-full text-gray-700 mr-3 px-2 leading-tight focus:outline-none" type="text" placeholder="Name" name="username" aria-label="Full name" />
+                <textarea className="appearance-none bg-transparent border-none sm:30 md:h-60 lg:h-80 w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="Your comment..." name="comment" aria-label="Comment"></textarea>
+                <button className="flex-shrink-0 bg-teal hover:bg-nonphotblue hover:bg-photo text-sm text-white py-1 px-2 rounded" type="submit">
+                    Add Comment
+                </button>
+            </div>
+        </form>
+    )
+}
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,12 +1,13 @@
 import { WEBSITE_URL } from "config"
 
 import { dancing_script } from "@/app/layout";
+import CommentForm from "./CommentForm";
 
 export default async function Comments({ slug }: { slug: string }) {
     let comments = [];
 
     try {
-        const commentsRes = await fetch(`${WEBSITE_URL}/api/comments/${slug}`, { next: { revalidate: 5 } });
+        const commentsRes = await fetch(`${WEBSITE_URL}/api/comments/${slug}`, { next: { revalidate: 1 } });
         comments = await commentsRes.json();
     } catch (err) {
         console.log(err)
@@ -16,15 +17,10 @@ export default async function Comments({ slug }: { slug: string }) {
     return (
         <div className="mx-auto px-4 sm:px-6 md:w-3/4  lg:w-2/3 xl:w-1/2">
             <h3 className={`${dancing_script.className} text-6xl font-bold text-teal -skew-y-3 tracking-tighter m-4`}>Comments</h3>
-            <form className="w-full mt-10" method="POST" action={`/api/comments/${slug}`}>
-                <div className="flex flex-col gap-4 items-center border-b border-teal py-2">
-                    <input className="appearance-none bg-transparent border-b border-teal py-2 w-full text-gray-700 mr-3 px-2 leading-tight focus:outline-none" type="text" placeholder="Name" name="username" aria-label="Full name" />
-                    <textarea className="appearance-none bg-transparent border-none sm:30 md:h-60 lg:h-80 w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="Your comment..." name="comment" aria-label="Comment"></textarea>
-                    <button className="flex-shrink-0 bg-teal hover:bg-nonphotblue hover:bg-photo text-sm text-white py-1 px-2 rounded" type="submit">
-                        Add Comment
-                    </button>
-                </div>
-            </form>
+            
+            {/* Comment Form Component Here */}
+
+            <CommentForm slug={slug}/>
 
             <ul className="w-full">
                 {/* @ts-ignore */}
